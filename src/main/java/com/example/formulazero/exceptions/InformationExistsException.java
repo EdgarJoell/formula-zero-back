349627c8602f30dd5d60364e5ce71dd299e48a61
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-05-31 17:12:35 -0400

    Write InformationExistsException

--- src/main/java/com/example/formulazero/exceptions/InformationExistsException.java
+++ src/main/java/com/example/formulazero/exceptions/InformationExistsException.java
@@ -1,4 +1,11 @@
 package com.example.formulazero.exceptions;
 
-public class InformationExistsException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class InformationExistsException extends RuntimeException{
+    public InformationExistsException(String message) {
+        super(message);
+    }
 }
