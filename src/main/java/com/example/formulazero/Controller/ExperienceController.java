fa990c8ddf3379ac2f9c1d925e8f05baef5a2680
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-05-31 16:54:20 -0400

    Make ExperienceController a RestController

--- src/main/java/com/example/formulazero/Controller/ExperienceController.java
+++ src/main/java/com/example/formulazero/Controller/ExperienceController.java
@@ -1,4 +1,10 @@
 package com.example.formulazero.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/")
 public class ExperienceController {
+
 }
