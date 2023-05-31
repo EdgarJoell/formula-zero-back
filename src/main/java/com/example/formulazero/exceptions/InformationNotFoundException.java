496eb7ac5a51a27401e85948889d9f268f1a4952
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-05-31 17:13:29 -0400

    Write InformationNotFoundException

--- src/main/java/com/example/formulazero/exceptions/InformationNotFoundException.java
+++ src/main/java/com/example/formulazero/exceptions/InformationNotFoundException.java
@@ -1,4 +1,11 @@
 package com.example.formulazero.exceptions;
 
-public class InformationNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class InformationNotFoundException extends RuntimeException{
+    public InformationNotFoundException(String message) {
+        super(message);
+    }
 }
