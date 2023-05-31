5772f379cf37c1d505250e6312e67079f838fd29
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-05-31 19:02:05 -0400

    Begin writing seed data

--- src/main/java/com/example/formulazero/seed/DataLoader.java
+++ src/main/java/com/example/formulazero/seed/DataLoader.java
@@ -1,4 +1,25 @@
 package com.example.formulazero.seed;
 
-public class DataLoader {
+import com.example.formulazero.Repository.ExperienceRepository;
+import com.example.formulazero.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+    @Autowired
+    ExperienceRepository experienceRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadUserData();
+    }
+
+    private void loadUserData() {
+
+    }
 }
