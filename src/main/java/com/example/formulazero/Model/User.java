7da3f1d97d87a7f84c5bf17646cfe66368b08990
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-05-31 16:19:38 -0400

    Create User model. Write User model

---
+++ src/main/java/com/example/formulazero/Model/User.java
@@ -0,0 +1,102 @@
+package com.example.formulazero.Model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    private String name;
+
+    @Column
+    private String homeCountry;
+
+    @Column
+    private String favoriteTeam;
+
+    @Column
+    private String favoriteDriver;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHomeCountry() {
+        return homeCountry;
+    }
+
+    public void setHomeCountry(String homeCountry) {
+        this.homeCountry = homeCountry;
+    }
+
+    public String getFavoriteTeam() {
+        return favoriteTeam;
+    }
+
+    public void setFavoriteTeam(String favoriteTeam) {
+        this.favoriteTeam = favoriteTeam;
+    }
+
+    public String getFavoriteDriver() {
+        return favoriteDriver;
+    }
+
+    public void setFavoriteDriver(String favoriteDriver) {
+        this.favoriteDriver = favoriteDriver;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", homeCountry='" + homeCountry + '\'' +
+                ", favoriteTeam='" + favoriteTeam + '\'' +
+                ", favoriteDriver='" + favoriteDriver + '\'' +
+                '}';
+    }
+}
