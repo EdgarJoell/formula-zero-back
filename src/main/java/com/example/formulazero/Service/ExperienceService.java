4d81ba35abb59ec8b066859644f1402d283bcdb3
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-05-31 17:06:23 -0400

    Connect and Autowire Experience Controller, Service, and Repository

--- src/main/java/com/example/formulazero/Service/ExperienceService.java
+++ src/main/java/com/example/formulazero/Service/ExperienceService.java
@@ -1,7 +1,15 @@
 package com.example.formulazero.Service;
 
+import com.example.formulazero.Repository.ExperienceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ExperienceService {
+    private ExperienceRepository experienceRepository;
+
+    @Autowired
+    public void setExperienceRepository(ExperienceRepository experienceRepository) {
+        this.experienceRepository = experienceRepository;
+    }
 }
