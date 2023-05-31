0daf06e79a7b5daf766ac77be1217625cabcdab1
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-05-31 16:06:17 -0400

    Write Experience model

--- src/main/java/com/example/formulazero/Model/Experience.java
+++ src/main/java/com/example/formulazero/Model/Experience.java
@@ -1,4 +1,90 @@
 package com.example.formulazero.Model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "experiences")
 public class Experience {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String trackName;
+
+    @Column
+    private String date;
+
+    @Column
+    private double price;
+
+    @Column
+    private String time;
+
+    @Column
+    private String car;
+
+    public Experience() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public void setTrackName(String trackName) {
+        this.trackName = trackName;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getCar() {
+        return car;
+    }
+
+    public void setCar(String car) {
+        this.car = car;
+    }
+
+    @Override
+    public String toString() {
+        return "Experience{" +
+                "id=" + id +
+                ", trackName='" + trackName + '\'' +
+                ", date='" + date + '\'' +
+                ", price=" + price +
+                ", time='" + time + '\'' +
+                ", car='" + car + '\'' +
+                '}';
+    }
 }
