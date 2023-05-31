d048240856b99dabac2a4221f0e790b68cadd71f
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-05-31 17:07:37 -0400

    Connect and Autowire UserController, Service, and Repository

--- src/main/java/com/example/formulazero/Controller/UserController.java
+++ src/main/java/com/example/formulazero/Controller/UserController.java
@@ -1,9 +1,17 @@
 package com.example.formulazero.Controller;
 
+import com.example.formulazero.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/auth/")
 public class UserController {
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 }
