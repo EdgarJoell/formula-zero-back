92eafad3b893fc42d74e13ebab0f4e45f655d4b7
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-05-31 11:32:13 -0400

    first commit

---
+++ src/main/java/com/example/formulazero/FormulaZeroApplication.java
@@ -0,0 +1,13 @@
+package com.example.formulazero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormulaZeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormulaZeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/formulazero/FormulaZeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.formulazero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormulaZeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
