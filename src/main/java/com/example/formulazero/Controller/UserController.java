639ae25e2bf91c8c06c14e6b012783fac46fc161
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-05-31 16:55:09 -0400

    Make UserController a RestController

--- src/main/java/com/example/formulazero/Controller/UserController.java
+++ src/main/java/com/example/formulazero/Controller/UserController.java
@@ -1,4 +1,9 @@
 package com.example.formulazero.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/auth/")
 public class UserController {
 }
