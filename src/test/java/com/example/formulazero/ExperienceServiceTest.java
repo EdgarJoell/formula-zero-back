76c9ad0a4621001b9f7afc121f09fab5e2b01552
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-05-31 20:31:29 -0400

    Write test to get all available Experiences

--- src/test/java/com/example/formulazero/ExperienceServiceTest.java
+++ src/test/java/com/example/formulazero/ExperienceServiceTest.java
@@ -1,4 +1,15 @@
 package com.example.formulazero;
 
+import com.example.formulazero.Service.ExperienceService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class ExperienceServiceTest {
+    private ExperienceService experienceService;
+
+    @Test
+    @DisplayName("Get a list of all Experiences")
+    public final void getAListOfAllExperiences() {
+        experienceService.getExperiences();
+    }
 }
