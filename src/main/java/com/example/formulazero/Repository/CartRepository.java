060f8fc034665338e5dd7b641648e3fea5e0045c
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-06-06 15:50:21 -0400

    Setup Cart Repository

--- src/main/java/com/example/formulazero/Repository/CartRepository.java
+++ src/main/java/com/example/formulazero/Repository/CartRepository.java
@@ -1,4 +1,7 @@
 package com.example.formulazero.Repository;
 
-public class CartRepository {
+import com.example.formulazero.Model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
 }
