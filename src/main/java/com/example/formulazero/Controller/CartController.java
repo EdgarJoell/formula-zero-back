78fdfdbae59664e34f159ca045402d9c4d953d84
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-06-06 15:48:22 -0400

    Set up Cart Controller

--- src/main/java/com/example/formulazero/Controller/CartController.java
+++ src/main/java/com/example/formulazero/Controller/CartController.java
@@ -1,4 +1,19 @@
 package com.example.formulazero.Controller;
 
+import com.example.formulazero.Service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/cart/")
 public class CartController {
+    private CartService cartService;
+
+    @Autowired
+    public void setCartService(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+
 }
