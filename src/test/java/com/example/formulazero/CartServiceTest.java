702c9fcbec16d63ffc31a1d017942bef2c241d13
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-06-06 16:15:44 -0400

    Create and write CartServiceTest class

---
+++ src/test/java/com/example/formulazero/CartServiceTest.java
@@ -0,0 +1,11 @@
+package com.example.formulazero;
+
+import com.example.formulazero.Service.ExperienceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CartServiceTest {
+    @Autowired
+    private ExperienceService experienceService;
+}
