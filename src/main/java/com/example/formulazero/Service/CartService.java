fbd190aaf68f9d4795cb619624ca62b7dc79feb0
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-06-06 15:49:29 -0400

    Setup Cart Service

--- src/main/java/com/example/formulazero/Service/CartService.java
+++ src/main/java/com/example/formulazero/Service/CartService.java
@@ -1,4 +1,17 @@
 package com.example.formulazero.Service;
 
+import com.example.formulazero.Repository.CartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CartService {
+    private CartRepository cartRepository;
+
+    @Autowired
+    public void setCartRepository(CartRepository cartRepository) {
+        this.cartRepository = cartRepository;
+    }
+
+
 }
